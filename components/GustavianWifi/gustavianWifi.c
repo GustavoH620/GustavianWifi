4e38fd60c7d3581dfe6d911dfa970504c58aaa72
Gustavo C <137294905+GustavoH620@users.noreply.github.com>
2026-08-07 18:12:00 -0300

    Correção de lógica, provisionamento não iniciado quando rede anterior não era encontrada.

--- components/GustavianWifi/gustavianWifi.c
+++ components/GustavianWifi/gustavianWifi.c
@@ -59,12 +59,23 @@ void gustavianWifiStart(){
     configurar_wifi();
 
     vTaskDelay(pdMS_TO_TICKS(1000));
+    int ultima_rede = checar_ultima_rede();
+    if (ultima_rede){
+        ESP_LOGI("MAIN", "Última rede não encontrada, iniciando provisionamento...");
+        provisionamentoWifiHTTP();
 
-    conectar_ultima_rede();
+    } else {
+        conectar_ultima_rede();
+    }
+    
 
     vTaskDelay(pdMS_TO_TICKS(5000));
 
-    if (conexao) ESP_LOGI("MAIN", "Rede conectada!, continuando rotina...");
+    if (conexao) {
+        ESP_LOGI("MAIN", "Rede conectada!, continuando rotina...");
+    } else {
+        ESP_LOGI("MAIN", "Rede não conectada...");
+    }
 
     xTaskCreate(
         task_intr_wifi,
